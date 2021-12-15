34dd301dd8c5716fcd85dff712d7420f9ebebd91
Evgeny Zhdanov <evgeny1halvin@yandex.ru>
2021-12-15 02:07:09 +0200

    chore: изменил название групп colors

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,10 +1,10 @@
 export const theme = {
   colors: {
-    bg: {
+    main: {
       primary: { normal: '#0A0A0A', hover: '#292929' },
       secondary: { normal: '#EC4926', hover: '' },
     },
-    text: {
+    detailed: {
       primary: { normal: '#FAFAFA', hover: '' },
       secondary: { normal: '#999999', hover: '' },
       success: { normal: '#26EC26', hover: '' },
