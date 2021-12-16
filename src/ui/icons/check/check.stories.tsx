75c278a4a5ba444c2efdcc8c4bcb088b85d4a4cf
Skovi <skovi196@gmail.com>
2021-12-16 14:20:24 +0700

    2/feat:исправила замечания

---
+++ src/ui/icons/check/check.stories.tsx
@@ -0,0 +1,11 @@
+import { Check } from "./check";
+
+export default {
+  title: "icons/Check",
+  component: Check,
+  args: {
+    color: "#FAFAFA",
+  },
+};
+
+export const Basic = Check;
---
+++ src/ui/icons/copy/copy.stories.tsx
@@ -0,0 +1,11 @@
+import { Copy } from "./copy";
+
+export default {
+  title: "icons/Copy",
+  component: Copy,
+  args: {
+    color: "#FAFAFA",
+  },
+};
+
+export const Basic = Copy;
