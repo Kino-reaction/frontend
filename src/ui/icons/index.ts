4d63737144518ce7bbc18ce0b91e70d479580c73
Sergey Kolesnikov <sergey@829llc.com>
2021-12-17 19:02:07 +0300

    1/feat: добавили иконки Like, Edit, Settings, Trash как атомы

--- src/ui/icons/index.ts
+++ src/ui/icons/index.ts
@@ -1,4 +1,8 @@
 export { LikeFilled } from "./like-filled";
+export { Like } from "./like";
+export { Edit } from "./edit";
+export { Settings } from "./settings";
+export { Trash } from "./trash";
 export { Home } from "./home";
 export { ChevronsLeft } from "./chevrons-left";
 export { ChevronsRight } from "./chevrons-right";
---
+++ src/ui/icons/trash/trash.stories.tsx
@@ -0,0 +1,11 @@
+import { Trash } from "./trash";
+
+export default {
+  title: "icons/Trash",
+  component: Trash,
+  args: {
+    color: "#FAFAFA",
+  },
+};
+
+export const Basic = Trash;
